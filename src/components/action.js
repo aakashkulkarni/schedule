fdde8f13b4f351fd6d23a21686718fd4684ee6e1
aakashkulkarni <kashakoolsmart@gmail.com>
2018-07-26 10:26:36 -0600

    added logic to toggle course reducer

--- src/components/action.js
+++ src/components/action.js
@@ -5,7 +5,7 @@ import Icon from './icon';
 class Action extends Component {
     render() {
         return(
-            <button onClick={() => this.props.onClick} className={`${this.props.className} action`}>
+            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
                 { Icon('fas fa-plus-circle') }
                 {/* fas fa-times-circle */}
             </button> 
